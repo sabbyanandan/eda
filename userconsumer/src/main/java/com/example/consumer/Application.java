25bd67e95bfce185b49165facfc113dd00d7ac95
Soby Chacko <schacko@pivotal.io>
2018-09-11 12:19:55 -0400

    Interactive query support

--- userconsumer/src/main/java/com/example/consumer/Application.java
+++ userconsumer/src/main/java/com/example/consumer/Application.java
@@ -6,12 +6,23 @@ import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Serialized;
 import org.apache.kafka.streams.kstream.TimeWindows;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyWindowStore;
+import org.apache.kafka.streams.state.WindowStoreIterator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.binder.kafka.streams.QueryableStoreRegistry;
 import org.springframework.kafka.support.serializer.JsonSerde;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.StringJoiner;
+import java.util.concurrent.atomic.AtomicLong;
 
 @SpringBootApplication
 @EnableBinding(UserChannels.class)
@@ -19,7 +30,12 @@ public class Application {
 
 	public static final int WINDOW_SIZE = 30000;
 
+	public static final String USERS_GROUPED_BY_REGION_SNAPSHOT = "users-grouped-by-region-snapshot";
+
 	private final CommandPublisher commandPublisher;
+
+	@Autowired
+	private QueryableStoreRegistry queryableStoreRegistry;
 
 	public Application(CommandPublisher commandPublisher) {
 		this.commandPublisher = commandPublisher;
@@ -57,14 +73,44 @@ public class Application {
 				.groupByKey(Serialized
 						.with(new Serdes.StringSerde(), new JsonSerde<>(DomainEvent.class)))
 				.windowedBy(TimeWindows.of(WINDOW_SIZE))
-				.count(Materialized.as("users-grouped-by-region-snapshot"))
+				.count(Materialized.as(USERS_GROUPED_BY_REGION_SNAPSHOT))
 				.toStream()
-				.map((k, v) -> {
-					System.out
-							.println(
-									"In the last" + WINDOW_SIZE + " ms, " + v + " new Users were CREATED in the " + k
-											+ " Region.");
-					return new KeyValue(null, new UsersByRegionCount(k.key(), v, k.window().start(), k.window().end()));
-				});
+				.map((k, v) -> new KeyValue<>(null, new UsersByRegionCount(k.key(), v, k.window().start(), k.window().end())));
 	}
+
+	@RestController
+	class InteractiveQueryController {
+
+		@RequestMapping("/windows")
+		public String windowedData(@RequestParam(value="region") String region) {
+
+			ReadOnlyWindowStore<String, Long> queryableStore = queryableStoreRegistry.getQueryableStoreType(USERS_GROUPED_BY_REGION_SNAPSHOT,
+					QueryableStoreTypes.windowStore());
+			long now = System.currentTimeMillis();
+			WindowStoreIterator<Long> regionCountIerator = queryableStore.fetch(region, now - (60 * 1000 * 5), now);
+
+			StringJoiner regionCounts = new StringJoiner(", ", "[", "]");
+
+			regionCountIerator.forEachRemaining(value -> regionCounts.add(String.valueOf(value.value)));
+
+			return regionCounts.toString();
+		}
+
+		@RequestMapping("/windows/cumulative")
+		public long windowedCumulative(@RequestParam(value="region") String region) {
+
+			ReadOnlyWindowStore<String, Long> queryableStore = queryableStoreRegistry.getQueryableStoreType(USERS_GROUPED_BY_REGION_SNAPSHOT,
+					QueryableStoreTypes.windowStore());
+			long now = System.currentTimeMillis();
+			WindowStoreIterator<Long> regionCountIerator = queryableStore.fetch(region, now - (60 * 1000 * 60), now);
+
+			AtomicLong total = new AtomicLong(0);
+
+			regionCountIerator.forEachRemaining(value -> total.addAndGet(value.value));
+
+			return total.get();
+		}
+
+	}
+
 }
